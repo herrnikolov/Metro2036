0f4c25dd9d9afac019be913bb5ab6a59dde0aed6
Stefan Nikolov <herr.nikolov@gmail.com>
2018-08-15 17:14:33 +0300

    Add Services Project

---
+++ Metro2036.Services/Implementations/StationService.cs
@@ -0,0 +1,48 @@
+﻿using Metro2036.Data;
+using Metro2036.Models;
+using Metro2036.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Metro2036.Services.Implementations
+{
+    public class StationService : BaseService, IStationService
+    {
+        private Metro2036DbContext _context;
+
+        //public StationService(Metro2036DbContext db) : base(db) { }
+
+        public StationService(Metro2036DbContext context)
+        {
+            _context = context;
+        }
+
+        public Station Add(Station station)
+        {
+            _context.Stations.Add(station);
+            _context.SaveChanges();
+            return station;
+        }
+
+        public Station Get(int id)
+        {
+            return _context.Stations.FirstOrDefault(s => s.Id == id);
+        }
+
+        public IEquatable<Station> GetAll()
+        {
+            return _context.Stations.OrderBy(s => s.Name);
+        }
+
+        public Station Update(Station station)
+        {
+            _context.Attach(station).State =
+                EntityState.Modified;
+            _context.SaveChanges();
+            return station;
+        }
+    }
+}
---
+++ Metro2036.Services/Interfaces/IStationService.cs
@@ -0,0 +1,17 @@
+﻿namespace Metro2036.Services.Interfaces
+{
+    using Metro2036.Models;
+    using System;
+    using System.Collections.Generic;
+
+    public interface IStationService
+    {
+        IEnumerable<Station> GetAll();
+
+        Station Get(int id);
+
+        Station Add(Station station);
+
+        Station Update(Station station);
+    }
+}
