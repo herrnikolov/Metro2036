c6460ba699226b8d0696ceee220adcfd929ab091
Stefan Nikolov <herr.nikolov@gmail.com>
2018-09-27 13:50:46 +0300

    wip

--- Metro2036.DevOps/vagrant/jenkins/jenkins-add-credentials.sh
+++ Metro2036.DevOps/vagrant/jenkins/jenkins-add-credentials.sh
@@ -4,7 +4,8 @@
 # Create Jenkins credentials
 # 
 
-java -jar /opt/jenkins-cli.jar -s http://localhost:8080/ -remoting login --username admin --password admin && echo '<com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl>                                      
+java -jar /opt/jenkins-cli.jar -s http://localhost:8080/ -remoting login --username admin --password admin && echo 
+'<com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl>                                      
 <scope>GLOBAL</scope>
   <id>vagrant</id>
   <description>Vagrant username and password pair</description>
